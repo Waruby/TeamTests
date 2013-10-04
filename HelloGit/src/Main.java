e4d99ae9bbd69d56bc1ada870be7c4ae2d2e9b69
Waruby <waruby@hotmail.fr>
2013-10-04 14:09:54 +0200

    on ajoute une fonction affln pour aller plus vite

--- HelloGit/src/Main.java
+++ HelloGit/src/Main.java
@@ -2,7 +2,11 @@
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("Oh Yann !");
+		System.out.println("Putain ! Pas la constante de plank !");
 
+	}
+	
+	public static void affln(Object o){
+		System.out.println(o);
 	}
 }
